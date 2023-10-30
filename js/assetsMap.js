e9ebfb1a88bc1807008f5cbecb8efa05d27a31c8
darya-yarosh <gurimusagi@yandex.ru>
2023-10-30 18:58:54 +0300

    Formatted code.

--- js/assetsMap.js
+++ js/assetsMap.js
@@ -1,32 +1,3 @@
-// import LightShell from '../assets/parts/bullets/LightShell.png';
-// import MediumShell from '../assets/parts/bullets/MediumShell.png';
-// import HeavyShell from '../assets/parts/bullets/HeavyShell.png';
-
-// import GunConnectorA from '../assets/parts/gun_connectors/GunConnectorA.png';
-// import GunConnectorB from '../assets/parts/gun_connectors/GunConnectorB.png';
-// import GunConnectorD from '../assets/parts/gun_connectors/GunConnectorD.png';
-
-// import SmallGunA from '../assets/parts/guns/SmallGunA.png';
-// import MediumGunA from '../assets/parts/guns/MediumGunA.png';
-// import HeavyGunB from '../assets/parts/guns/HeavyGunB.png';
-
-// import SmallHullA from '../assets/parts/hulls/SmallHullA.png';
-// import MediumHullA from '../assets/parts/hulls/MediumHullA.png';
-// import HeavyHullB from '../assets/parts/hulls/HeavyHullB.png';
-
-// import MapTileA from '../assets/parts/map_tiles/A.png';
-
-// import SmallTowerA from '../assets/parts/towers/SmallTowerA.png';
-// import MediumTowerA from '../assets/parts/towers/MediumTowerA.png';
-// import HeavyTowerB from '../assets/parts/towers/HeavyTowerB.png';
-
-// import TrackAFrame1 from '../assets/parts/tracks/TrackAFrame1.png';
-// import TrackAFrame2 from '../assets/parts/tracks/TrackAFrame2.png';
-// import TrackBFrame1 from '../assets/parts/tracks/TrackBFrame1.png';
-// import TrackBFrame2 from '../assets/parts/tracks/TrackBFrame2.png';
-// import TrackCFrame1 from '../assets/parts/tracks/TrackCFrame1.png';
-// import TrackCFrame2 from '../assets/parts/tracks/TrackCFrame2.png';
-
 const assetsMap = {
     sprites: [
         {name: "LightShell", url: '../assets/parts/bullets/LightShell.png'},
--- js/index.js
+++ js/index.js
@@ -5,63 +5,38 @@ import assetsMap from "./assetsMap.js";
 
 import Tank from "./Tank.js";
 
-// Create the application
-const app = new Application({
+const colors = {
+   black: 0x000000,
+   gray: 0xc2c2c2,
+   red: 0xff0000,
+}
+
+const SCREEN_SIZE = {
    width: 800,
    height: 800,
-   backgroundColor: 0xc2c2c2,
+}
+
+// Create the application
+const app = new Application({
+   width: SCREEN_SIZE.width,
+   height: SCREEN_SIZE.height,
+   backgroundColor: colors.gray,
    view: document.getElementById("canvas"),
 });
-
-// Add the view to the DOM
-//document.body.appendChild(app.view);
 
 const runGame = () => {
    app.stage.position.set(800 / 2, 800 / 2);
 
    const marker = new Graphics();
-   marker.beginFill(0xff0000, 1);
-   //marker.drawRect(0,0,10,10);
+   marker.beginFill(colors.red, 1);
    marker.drawCircle(0, 0, 5);
    marker.endFill();
-   //window["RECTANGLE"] = marker;
 
    const tank = new Tank();
-   //tank.view.visible = false;
    app.stage.addChild(tank.view);
    app.stage.addChild(marker);
    
    window["TANK"] = tank;
-
-   // --- Ticker --- START ---
-   // app.ticker.add(()=>{
-   //    console.log("Ticker - описание его свойств")
-   //    console.log("lastTime - время жизни с момента старта", app.ticker.lastTime);
-   //    console.log("deltaTime = lastTime - lastTimePrev", app.ticker.deltaTime)
-   //    console.log("deltaMS = (lastTime - lastTimePrev) / (1000 / 60 {1 секунда / 60 кадров в секунду (FPS)} )", app.ticker.deltaMS);
-   // })
-
-   // let value = 0;
-   // const stepValue = 0.11;
-   // const offset = 200;
-   // app.ticker.add(()=>{
-   //     value += stepValue;
-   //     //rectangle.alpha = Math.cos(value);
-   //     rectangle.position.x = offset * Math.cos(value);
-   // })
-
-   // --- Ticker --- END ---
-
-//    const onPointerDown = ({ data }) => {
-//       const positions = data.getLocalPosition(app.stage);
-//       console.log(positions)
-//       app.stage.addChild(
-//           new Graphics()
-//           .beginFill(colors.red, 1)
-//           .drawCircle(positions.x, positions.y, 5)
-//           .endFill()
-//       );
-//   };
 
    const tweenManager = new TweenManager(app.ticker);
 
@@ -119,13 +94,13 @@ const runGame = () => {
    }
 
    app.stage.on("pointerdown", moveTank, undefined);
-   // для отслеживания нажатий
    app.stage.interactive = true;
-   // проводим проверку только в канвас, не нужно проверять все дочерние компоненты 
    app.stage.interactiveChildren = false;
    app.stage.hitArea = new Rectangle(-400, -400, 800, 800);
 
 }
 
+// Loading assets
 assetsMap.sprites.forEach(value => app.loader.add(value.name, value.url));
+// Start app
 app.loader.load(runGame);
