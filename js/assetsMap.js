5499fca549b483bd110c5b37332b6711434c6211
darya-yarosh <gurimusagi@yandex.ru>
2023-10-30 17:11:21 +0300

    Loaded map of assets

---
+++ js/assetsMap.js
@@ -0,0 +1,63 @@
+// import LightShell from '../assets/parts/bullets/LightShell.png';
+// import MediumShell from '../assets/parts/bullets/MediumShell.png';
+// import HeavyShell from '../assets/parts/bullets/HeavyShell.png';
+
+// import GunConnectorA from '../assets/parts/gun_connectors/GunConnectorA.png';
+// import GunConnectorB from '../assets/parts/gun_connectors/GunConnectorB.png';
+// import GunConnectorD from '../assets/parts/gun_connectors/GunConnectorD.png';
+
+// import SmallGunA from '../assets/parts/guns/SmallGunA.png';
+// import MediumGunA from '../assets/parts/guns/MediumGunA.png';
+// import HeavyGunB from '../assets/parts/guns/HeavyGunB.png';
+
+// import SmallHullA from '../assets/parts/hulls/SmallHullA.png';
+// import MediumHullA from '../assets/parts/hulls/MediumHullA.png';
+// import HeavyHullB from '../assets/parts/hulls/HeavyHullB.png';
+
+// import MapTileA from '../assets/parts/map_tiles/A.png';
+
+// import SmallTowerA from '../assets/parts/towers/SmallTowerA.png';
+// import MediumTowerA from '../assets/parts/towers/MediumTowerA.png';
+// import HeavyTowerB from '../assets/parts/towers/HeavyTowerB.png';
+
+// import TrackAFrame1 from '../assets/parts/tracks/TrackAFrame1.png';
+// import TrackAFrame2 from '../assets/parts/tracks/TrackAFrame2.png';
+// import TrackBFrame1 from '../assets/parts/tracks/TrackBFrame1.png';
+// import TrackBFrame2 from '../assets/parts/tracks/TrackBFrame2.png';
+// import TrackCFrame1 from '../assets/parts/tracks/TrackCFrame1.png';
+// import TrackCFrame2 from '../assets/parts/tracks/TrackCFrame2.png';
+
+const assetsMap = {
+    sprites: [
+        {name: "LightShell", url: '../assets/parts/bullets/LightShell.png'},
+        {name: "MediumShell", url: '../assets/parts/bullets/MediumShell.png'},
+        {name: "HeavyShell", url: '../assets/parts/bullets/HeavyShell.png'},
+
+        {name: "GunConnectorA", url: '../assets/parts/gun_connectors/GunConnectorA.png'},
+        {name: "GunConnectorB", url: '../assets/parts/gun_connectors/GunConnectorB.png'},
+        {name: "GunConnectorD", url: '../assets/parts/gun_connectors/GunConnectorD.png'},
+
+        {name: "SmallGunA", url: '../assets/parts/guns/SmallGunA.png'},
+        {name: "MediumGunA", url: '../assets/parts/guns/MediumGunA.png'},
+        {name: "HeavyGunB", url: '../assets/parts/guns/HeavyGunB.png'},
+
+        {name: "SmallHullA", url: '../assets/parts/hulls/SmallHullA.png'},
+        {name: "MediumHullA", url: '../assets/parts/hulls/MediumHullA.png'},
+        {name: "HeavyHullB", url: '../assets/parts/hulls/HeavyHullB.png'},
+
+        {name: "MapTileA", url: '../assets/parts/map_tiles/A.png'},
+
+        {name: "SmallTowerA", url: '../assets/parts/towers/SmallTowerA.png'},
+        {name: "MediumTowerA", url: '../assets/parts/towers/MediumTowerA.png'},
+        {name: "HeavyTowerB", url: '../assets/parts/towers/HeavyTowerB.png'},
+
+        {name: "TrackAFrame1", url: '../assets/parts/tracks/TrackAFrame1.png'},
+        {name: "TrackAFrame2", url: '../assets/parts/tracks/TrackAFrame2.png'},
+        {name: "TrackBFrame1", url: '../assets/parts/tracks/TrackBFrame1.png'},
+        {name: "TrackBFrame2", url: '../assets/parts/tracks/TrackBFrame2.png'},
+        {name: "TrackCFrame1", url: '../assets/parts/tracks/TrackCFrame1.png'},
+        {name: "TrackCFrame2", url: '../assets/parts/tracks/TrackCFrame2.png'},
+    ]
+}
+
+export default assetsMap;
